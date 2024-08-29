56af93129ea17b51541742a9347fd6adbb91bd0f
Visvas Nagarajan <visvas333@gmail.com>
2024-08-29 20:58:06 +0530

    added crawl single page with error

--- crawl.js
+++ crawl.js
@@ -1,5 +1,27 @@
 const {JSDOM} = require('jsdom')
 
+async function crawlPage(currentUrl){
+    console.log(`Actively crawling: ${currentUrl}`)
+    try{
+        const resp = await fetch(currentUrl)
+        if(resp.status > 399){
+            console.log(`Having status code ${resp.status} on ${currentUrl}`)
+            return
+        }
+        const contentType = resp.headers.get("content-type")
+        if(!contentType.includes('text/html')){
+            console.log(`error in content type: ${contentType} on ${currentUrl}`)
+            return
+        }
+    console.log(await resp.text())
+    }catch(error){
+        console.log(`error in fetch : ${error.message} due to wrong URL : ${currentUrl}`)
+    }
+    
+}
+
+
+
 function getUrlsFromHtml(htmlBody,baseURL){
     const urls = []
     const dom = new JSDOM(htmlBody)
@@ -38,5 +60,6 @@ function normalizeUrl(urlString){
 
 module.exports = {
     normalizeUrl,
-    getUrlsFromHtml
+    getUrlsFromHtml,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+const { basename } = require("path")
+const { crawlPage } = require('./crawl.js')
+
+function main(){
+    if(process.argv.length < 3){
+        console.log("No website provided")
+        process.exit(1)
+    }else if(process.argv.length > 3){
+        console.log("too many command arguments")
+        process.exit(1)
+    }
+        baseUrl = process.argv[2]
+        console.log(`Starting to crawl ${baseUrl}`)
+        crawlPage(baseUrl)
+    
+
+}
+
+main()
