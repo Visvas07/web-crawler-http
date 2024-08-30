d7739b07043ca8b46a0695fddf35d3ad97486235
Visvas Nagarajan <visvas333@gmail.com>
2024-08-30 19:53:08 +0530

    Added the crawl mechanism for multi page in main.js

--- crawl.js
+++ crawl.js
@@ -1,23 +1,43 @@
 const {JSDOM} = require('jsdom')
 
-async function crawlPage(currentUrl){
+async function crawlPage(baseURL,currentUrl,pages){
+    
+
+    const baseUrlObj = new URL(baseURL)
+    const currentUrlObj = new URL(currentUrl)
+    if(baseUrlObj.hostname !== currentUrlObj.hostname){
+        return pages
+    }
+
+    const normalizedCurrentUrl = normalizeUrl(currentUrl)
+    if(pages[normalizedCurrentUrl] > 0){
+        pages[normalizedCurrentUrl]++
+        return pages
+    }
+
+    pages[normalizedCurrentUrl] = 1
     console.log(`Actively crawling: ${currentUrl}`)
+
     try{
         const resp = await fetch(currentUrl)
         if(resp.status > 399){
             console.log(`Having status code ${resp.status} on ${currentUrl}`)
-            return
+            return pages
         }
         const contentType = resp.headers.get("content-type")
         if(!contentType.includes('text/html')){
             console.log(`error in content type: ${contentType} on ${currentUrl}`)
-            return
+            return pages
         }
-    console.log(await resp.text())
+        const htmlBody = await resp.text()
+        const nextUrls = getUrlsFromHtml(htmlBody,baseURL)
+        for(const nextUrl of nextUrls){
+            pages = await crawlPage(baseURL,nextUrl,pages)
+        }
     }catch(error){
         console.log(`error in fetch : ${error.message} due to wrong URL : ${currentUrl}`)
     }
-    
+    return pages
 }
 
 
@@ -26,10 +46,13 @@ function getUrlsFromHtml(htmlBody,baseURL){
     const urls = []
     const dom = new JSDOM(htmlBody)
     const linkElements = dom.window.document.querySelectorAll("a")
+    console.log(linkElements.length)
     for(const linkElement of linkElements){
+       // console.log(linkElement.href)
         if(linkElement.href.slice(0,1) === '/'){
            try{
             const urlString = new URL(`${baseURL}${linkElement.href}`)
+            console.log(urlString.href)
             urls.push(urlString.href)
            }catch(error){
             console.log(`Invalid url :${error.message}`)
@@ -38,12 +61,14 @@ function getUrlsFromHtml(htmlBody,baseURL){
         }else{
             try{
                 const urlString = new URL(linkElement.href)
+                console.log(urlString.href)
                 urls.push(urlString.href)
                }catch(error){
                 console.log(`Invalid url :${error.message}`)
                }
         }
     }
+    console.log(urls.length)
     return urls
 
 }
--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-const { basename } = require("path")
 const { crawlPage } = require('./crawl.js')
 
-function main(){
+async function main(){
     if(process.argv.length < 3){
         console.log("No website provided")
         process.exit(1)
@@ -11,8 +10,10 @@ function main(){
     }
         baseUrl = process.argv[2]
         console.log(`Starting to crawl ${baseUrl}`)
-        crawlPage(baseUrl)
-    
+        const pages = await crawlPage(baseUrl,baseUrl,{})
+    for(const page of Object.entries(pages)){
+        console.log(page)
+    }
 
 }
 
